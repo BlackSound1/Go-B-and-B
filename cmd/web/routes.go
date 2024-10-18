22226ef836e93e1099e789449355a0fdbaaa6c44
BlackSound1 <matthewr@videotron.ca>
2024-10-18 19:38:51 -0400

    Populated pages and made them Go templates

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -20,7 +20,12 @@ func routes() http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
-
+	mux.Get("/contact", handlers.Repo.Contact)
+	mux.Get("/generals-quarters", handlers.Repo.Generals)
+	mux.Get("/majors-suite", handlers.Repo.Majors)
+	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Get("/make-reservation", handlers.Repo.Reservation)
+	
 	// Serve static files
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
