d58264cd64b99fc2281f7128f6bc9cbef5a7cfa1
BlackSound1 <matthewr@videotron.ca>
2024-10-24 19:32:26 -0400

    Migrations for seeding restrictions and rooms tables

---
+++ migrations/20241024232521_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,5 @@
+INSERT INTO 
+    rooms (room_name, created_at, updated_at) 
+VALUES
+	('General''s Quarters','2024-10-23 00:00:00.000','2024-10-23 00:00:00.000'),
+	('Major''s Suite','2024-10-23 00:00:00.000','2024-10-23 00:00:00.000');
---
+++ migrations/20241024233009_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,5 @@
+INSERT INTO 
+    restrictions (restriction_name, created_at, updated_at)
+VALUES
+	 ('Reservation','2024-10-23 00:00:00.000','2024-10-23 00:00:00.000'),
+	 ('Owner Block','2020-11-19 00:00:00.000','2020-11-19 00:00:00.000');
