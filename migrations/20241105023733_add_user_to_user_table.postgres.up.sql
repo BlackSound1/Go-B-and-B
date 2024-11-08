d748dd766417b760789c30588d37816b95962168
BlackSound1 <matthewr@videotron.ca>
2024-11-08 13:31:09 -0500

    migrations

---
+++ migrations/20241105023733_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO 
+    public.users (first_name, last_name, email, "password", access_level, created_at, updated_at)
+VALUES
+    ('Test', 'Mann', 'me@me.me', '$2a$12$mn3KzCM7Tfk7Ihz8VEvxl.924Pg4oOSPxmGmL1jrPH3wfr7PWUpvK', 3, '2024-11-28 00:00:00.000', '2024-11-28 00:00:00.000');
